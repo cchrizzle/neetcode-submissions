5350167d676dc816327084b4aa9d749d9fced3b5
Christopher Chang <64837366+cchrizzle@users.noreply.github.com>
2026-06-08 14:46:40 -0700

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) return false;
+
+        const sMap = {};
+
+        for (const letter of s) {
+            sMap[letter] = (sMap[letter] || 0) + 1;
+        }
+
+        for (const letter of t) {
+            if (!sMap[letter]) return false;
+            sMap[letter]--;
+        }
+
+        return true;
+    }
+}
