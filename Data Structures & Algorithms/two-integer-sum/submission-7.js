981a58b27a7079e15535735c1798e123145f7bf4
Christopher Chang <64837366+cchrizzle@users.noreply.github.com>
2026-07-06 11:52:05 -0700

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        if (nums.length === 2) return [0, 1];
+
+        const numsMap = new Map();
+
+        for (let i = 0; i < nums.length; i++) {
+            const diff = target - nums[i];
+
+            if (numsMap.has(diff)) return [i, numsMap.get(diff)];
+
+            numsMap.set(nums[i], i);
+        }
+    }
+}
