c2cddf73fe79a49ccad9075aeb67603c05307f80
Christopher Chang <64837366+cchrizzle@users.noreply.github.com>
2026-07-09 13:32:49 -0700

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        if (s.length % 2 !== 0 || s.length < 2) return false;
+
+        const validParentheses = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        };
+
+        const stack = [];
+
+        for (const char of s) {
+            if (char in validParentheses) {
+                if (stack.at(-1) !== validParentheses[char]) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            } else {
+                stack.push(char);
+            }
+        }
+
+        if (stack.length !== 0) return false;
+        return true;
+    }
+}
