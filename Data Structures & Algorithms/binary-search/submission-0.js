1064f6b3250613d96bc3f0e33a4bb131d0e2e28c
Christopher Chang <64837366+cchrizzle@users.noreply.github.com>
2026-07-13 13:16:08 -0700

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let low = 0;
+        let high = nums.length -1;
+
+        while (low < high) {
+            const mid = Math.floor((low + high) / 2)
+            const search = nums[mid]
+
+            if (search === target) return nums.indexOf(target)
+            if (search > target) high = mid - 1
+            else low = mid + 1
+        }
+
+        return -1;        
+    }
+}
