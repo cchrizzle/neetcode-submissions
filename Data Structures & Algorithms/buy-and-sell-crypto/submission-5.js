b1e058324c1391be069ade112fac927d755fcdd3
Christopher Chang <64837366+cchrizzle@users.noreply.github.com>
2026-07-19 14:31:50 -0700

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let left = 0;
+        let right = 1;
+        let maxProfit = 0;
+
+        while (right < prices.length) {
+            const diff = prices[right] - prices[left];
+
+            if (prices[right] < prices[left]) {
+                left = right;
+            } else {
+                if (diff > maxProfit) {
+                    maxProfit = diff;
+                }
+            }
+            right++
+        }
+
+        return maxProfit;
+    }
+}
