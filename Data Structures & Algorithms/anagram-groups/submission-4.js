e6c6e856f317d26a4ec79fd6490114cfa8c23cea
Christopher Chang <64837366+cchrizzle@users.noreply.github.com>
2026-07-19 13:36:25 -0700

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const res = {};
+
+        for (const str of strs) {
+            const values = new Array(26).fill(0);
+
+            for (const char of str) {
+                values[char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+            }
+
+            const key = values.join(',');
+
+            if (!res[key]) {
+                res[key] = [];
+            }
+
+            res[key].push(str);
+        }
+
+        return Object.values(res);
+    }
+}
